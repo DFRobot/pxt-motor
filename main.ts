92bcd08ce16eb6cd035ff070b0e4492d327acfa2
Jeroen Harmsen <jeroen.harmsen@nedap.com>
2019-03-15 15:14:46 +0100

    Dutch translation and adjusted PWM to 50Hz for Servo motors

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@
 /**
  *This is DFRobot:motor user motor and steering control function.
  */
-//% weight=10 color=#DF6721 icon="\uf013" block="DF-Driver"
+//% weight=10 color=#DF6721 icon="\uf013" block="DF-motorblok"
 namespace motor {
     const PCA9685_ADDRESS = 0x40
     const MODE1 = 0x00
@@ -241,9 +241,10 @@ namespace motor {
         if (!initialized) {
             initPCA9685()
         }
-        // 100hz
-        let v_us = (degree * 10 + 600) // 0.6ms ~ 2.4ms
-        let value = v_us * 4095 / (1000000 / 100)
+        // 50hz = 20ms
+        //let v_us = (degree * 1800 / 180 + 600)  // 0.6ms ~ 2.4ms
+		let v_us = (degree * 1000 / 180 +1000)    // 1.0ms ~ 2.0ms
+        let value = v_us * 4095 / 20000           //20ms
         setPwm(index + 7, 0, value)
     }
 
@@ -253,7 +254,7 @@ namespace motor {
      * speed(0~255).
     */
     //% weight=90
-    //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed"
+    //% blockId=motor_MotorRunExp block="Motor|%index|dir|%Dir|speed|%speed"
     //% speed.min=0 speed.max=255
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
@@ -496,7 +497,7 @@ namespace motor {
 	 * Stop the dc motor.
     */
     //% weight=20
-    //% blockId=motor_motorStop block="Motor stop|%index"
+    //% blockId=motor_motorStopExp block="Motor stop|%index"
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2 
     export function motorStop(index: Motors) {
         setPwm((4 - index) * 2, 0, 0);
@@ -507,7 +508,7 @@ namespace motor {
 	 * Stop all motors
     */
     //% weight=10
-    //% blockId=motor_motorStopAll block="Motor Stop All"
+    //% blockId=motor_motorStopAllExp block="Motor Stop All"
     export function motorStopAll(): void {
         for (let idx = 1; idx <= 4; idx++) {
             motorStop(idx);
